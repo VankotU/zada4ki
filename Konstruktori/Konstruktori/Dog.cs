3504eed8fac865502a2d0fefe4cc5b83699c7c44
VankotU <131190241+VankotU@users.noreply.github.com>
2023-10-05 13:00:19 +0300

    Add files via upload

---
+++ Konstruktori/Konstruktori/Dog.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konstruktori
+{
+    internal class Dog
+    {
+        private int age;
+        private string name;
+        private double kg;
+
+        public int Age
+        {
+            get { return age; } 
+            set { age = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double Kg
+        {
+            get { return kg; }
+            set { kg = value; }
+        }
+
+        public Dog()
+        {
+            this.age = 8;
+            this.name = "Rex";
+            this.kg = 50;
+        }
+
+        public Dog(int age):this()
+        {
+            this.age = age;
+        }
+
+        public Dog(int age, string name):this(age)
+        {
+            this.name = name;
+        }
+
+        public Dog(int age, string name, double kg):this(age,name)
+        {
+            this.kg = kg;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{age} {name} {kg}");
+        }
+    }
+}
---
+++ Konstruktori/Konstruktori/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konstruktori
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog1 = new Dog();
+            dog1.Print();
+
+            Console.Write("Vuvedi godini - ");
+            Dog dog2 = new Dog(int.Parse(Console.ReadLine()));
+            dog2.Print();
+
+            Console.Write("Vuvedi ime,godini - ");
+            Dog dog3 = new Dog(int.Parse(Console.ReadLine()), Console.ReadLine());
+            dog3.Print();
+
+            Console.Write("Vuvedi ime,godini - ");
+            Dog dog4 = new Dog(int.Parse(Console.ReadLine()), Console.ReadLine(), double.Parse(Console.ReadLine()));
+            dog4.Print();
+
+            List <Dog>izhod = new List<Dog> {dog1, dog2, dog3, dog4 };
+        }
+    }
+}
