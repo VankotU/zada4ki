1b216cf262ef6d4648c72b31d3e0e7f11ed52326
VankotU <131190241+VankotU@users.noreply.github.com>
2023-10-26 13:12:00 +0300

    Add files via upload

---
+++ AutoSalon/AutoSalon/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSalon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Vuvedi broika - ");
+                int n = int.Parse(Console.ReadLine());
+
+                Console.Write("Vuvedi danni za kolata - ");
+                List<Salon> list = new List<Salon>();
+
+                for (int i = 0; i < n; i++)
+                {
+                    var data = Console.ReadLine().Split();
+                    Salon s1 = new Salon(data[0], int.Parse(data[1]), double.Parse(data[2]), data[3]);
+                    list.Add(s1);
+                }
+
+                foreach (var s in list)
+                {
+                    s.Izhod();
+                }
+            }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}
---
+++ AutoSalon/AutoSalon/Salon.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSalon
+{
+    internal class Salon
+    {
+		private string brand;
+		private int age;
+		private double price;
+		private string color;
+
+		public string Color
+		{
+			get { return color; }
+			set 
+			{
+				if(string.IsNullOrEmpty(value))
+				{
+					throw new ArgumentNullException("Cvqt");
+				}
+				color = value;
+			}
+		}
+
+
+		public double Price
+		{
+			get { return price; }
+			set 
+			{ 
+				if(value > 5000 && value <10000)
+				{
+                    throw new ArgumentOutOfRangeException("Cena");
+                }
+				price = value;
+			}
+		}
+
+
+		public int Age
+		{
+			get { return age; }
+			set 
+			{
+				if(value > 10 && value < 15)
+				{
+                    throw new ArgumentOutOfRangeException("Godinki");
+                }
+				age = value;
+			}
+		}
+
+
+		public string Brand
+		{
+			get { return brand; }
+			set 
+			{
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("value");
+                }
+                brand = value;
+			}
+		}
+
+		public Salon(string brand, int age, double price, string color)
+		{
+			this.Brand = brand;
+			this.Age = age;
+			this.Price = price;
+			this.Color = color;
+		}
+
+		public void Izhod()
+		{
+			Console.WriteLine($"marka - {this.brand}: godini - {this.age}: cena - {this.price}: cvqt - {this.color}:");
+		}
+	}
+}
