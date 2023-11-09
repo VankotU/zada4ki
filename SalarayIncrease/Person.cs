127bcb6374df65bd11c763e5329aad07f5c471c9
VankotU <131190241+VankotU@users.noreply.github.com>
2023-11-09 13:17:55 +0200

    Add files via upload

---
+++ SalarayIncrease/Person.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalarayIncrease
+{
+    internal class Person
+    {
+		private string name;
+		private int age;
+		private double salary;
+		
+		public double Salary
+		{
+			get { return salary; }
+			set 
+			{
+                if (value < 1000)
+                {
+                    throw new ArgumentOutOfRangeException("Salary");
+                }
+                salary = value;
+			}
+		}
+
+
+		public int Age
+		{
+			get { return age; }
+			set 
+			{
+                if (value < 18 || value > 65)
+                {
+                    throw new ArgumentOutOfRangeException("Age");
+                }
+                age = value;
+			}
+		}
+
+
+		public  string Name
+		{
+			get { return name; }
+			set 
+			{
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new FormatException("name");
+                }
+                name = value;
+			}
+		}
+
+        public Person(string name, int age, double salary)
+		{
+			this.Name = name;
+			this.Age = age;
+			this.Salary = salary;
+		}
+
+		public void IncreasSalary(double percent)
+		{
+			if( this.age > 30 )
+			{
+				this.salary += this.salary * percent / 100;
+			}
+
+			else 
+			{
+                this.salary += this.salary * percent / 200;
+            }
+		}
+
+
+		public void Izhod()
+		{
+			Console.WriteLine($"Ime - {this.name}, Godini - {this.age}, Zaplata - {this.salary}:");
+		}
+	}
+}
---
+++ SalarayIncrease/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalarayIncrease
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                Console.WriteLine("Kolko choveka shte vuvejdash - ");
+                int n = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Vuvedi danni na rabotnika - ");
+                List <Person> list = new List<Person>(); 
+
+                for(int i = 0; i < n; i++)
+                {
+                    var data = Console.ReadLine().Split();
+                    Person w1 = new Person(data[0], int.Parse(data[1]), double.Parse(data[2]));
+                    list.Add(w1);
+                }
+
+                foreach(var item in list) 
+                {
+                    item.Izhod();
+                }
+
+                Console.WriteLine("Vuvedi uvelichenie na zaplata - ");
+                double percent = double.Parse(Console.ReadLine());
+
+                foreach(var item in list)
+                {
+                    item.IncreasSalary(percent);
+                }
+
+                foreach (var item in list)
+                {
+                    item.Izhod();
+                }
+            }
+
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
