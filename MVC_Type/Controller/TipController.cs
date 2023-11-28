9e1de129c450015bc2ccdfa9adeb6abb995b5bc9
VankotU <131190241+VankotU@users.noreply.github.com>
2023-11-28 14:05:18 +0200

    Add files via upload

---
+++ MVC_Type/Controller/TipController.cs
@@ -0,0 +1,26 @@
+﻿using MVC_Type.Model;
+using MVC_Type.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Type.Controller
+{
+    class TipController
+    {
+        private Tip tip = new Tip();
+        private Display display = new Display();
+
+        public TipController()
+        {
+            display.Input();
+            tip.Amount=display.Amount;
+            tip.Percent=display.Percent;
+            display.TipAmount= tip.CalculateTip();
+            display.TipAmount=tip.CalculateTotal();
+            display.Output();
+        }
+    }
+}
---
+++ MVC_Type/Model/Tip.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Type.Model
+{
+    class Tip
+    {
+		private double amount;
+		private double percent;
+
+		public double Amount
+		{
+			get { return amount; }
+			set { amount = value; }
+		}
+
+
+		public double Percent
+		{
+			get { return percent; }
+			set 
+			{
+				if(value>1)
+				{
+					percent = value/100.0;
+				}
+				else
+				{
+					percent = value;
+				}
+				percent = value;
+			}
+		}
+
+		public Tip(double amount, double percent)
+		{
+			this.Amount = amount;
+			this.Percent = percent;
+		}
+
+		public Tip() : this(0,0) { }
+
+		public double CalculateTip()
+		{
+			return Amount*Percent;
+		}
+
+        public double CalculateTotal()
+        {
+            return CalculateTip()+Amount;
+        }
+    }
+}
