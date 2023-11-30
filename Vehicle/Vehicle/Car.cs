1f115a906b477a4d88e26cfea26c2192f622ee21
VankotU <131190241+VankotU@users.noreply.github.com>
2023-11-30 13:10:35 +0200

    Add files via upload

---
+++ Vehicle/Vehicle/Car.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    public class Car
+    {
+		private string model;
+		private double price;
+		private int age;
+		private string number;
+
+		public string Number
+		{
+			get { return number; }
+			set { number = value; }
+		}
+
+
+		public int Age
+		{
+			get { return age; }
+			set { age = value; }
+		}
+
+
+		public double Price
+		{
+			get { return price; }
+			set { price = value; }
+		}
+
+
+		public string Model
+		{
+			get { return model; }
+			set { model = value; }
+		}
+
+		public Car(string model,double price,int age,string number) 
+		{
+			this.Age = age; 
+			this.Price=price;
+			this.Number = number;
+			this.Model = model;
+		}
+
+		public void Izhod()
+		{
+			Console.WriteLine($"Model - {this.model}, Price - {this.price}, Age - {this.age}, Number{this.number}");
+		}
+	}
+}
---
+++ Vehicle/Vehicle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> listCar = new List<Car>()
+            {
+                new Car("Mercedes", 3000, 2000, "RP4389CC"),
+                new Car("BMW", 4500, 1998, "PB9087RR"),
+                new Car("Lada", 2000, 1987, "OV9134CR"),
+                new Car("Scania", 15000, 2010, "PA8645KL"),
+            };
+
+            listCar.Where(x=>x.Price>2999.99).ToList().ForEach(x=>x.Izhod());
+            listCar.OrderByDescending(x=>x.Model).ThenBy(x=>x.Age).Take(3).ToList().ForEach(x=>Console.WriteLine(x.ToString()));
+            double min = listCar.Select(x=>x.Price).Min();
+            int maxYear = listCar.Select(x => x.Age).Min();
+
+            Console.WriteLine($"minimal price - {min:f2}");
+            Console.WriteLine($"oldest year - {maxYear}");
+
+
+        }
+    }
+}
