13a51ddef70af65c32f73104a6fea40674f5a25c
VankotU <131190241+VankotU@users.noreply.github.com>
2023-12-10 18:10:18 +0200

    Add files via upload

---
+++ MinerHW/MinerHW/DiamondMiner.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinerHW
+{
+    public class DiamondMiner
+    {
+        private string name;
+        private string typeOfDiamond;
+        private int totalNumber;
+
+        public int TotalNumber
+        {
+            get { return totalNumber; }
+            set
+            {
+                if (value < 100)
+                {
+                    throw new ArgumentOutOfRangeException("malko na broi");
+                }
+                totalNumber = value;
+            }
+        }
+
+
+        public string TypeOfDiamond
+        {
+            get { return typeOfDiamond; }
+            set { typeOfDiamond = value; }
+        }
+
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public DiamondMiner(string name, string typeOfDiamond, int totalNumber)
+        {
+            this.Name = name;
+            this.TypeOfDiamond = typeOfDiamond;
+            this.TotalNumber = totalNumber; 
+        }
+
+        public void Izhod()
+        {
+            Console.WriteLine($"ime - {this.name},tip - {this.typeOfDiamond},broika - {this.totalNumber}");
+        }
+    }
+}
---
+++ MinerHW/MinerHW/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinerHW
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n = int.Parse(Console.ReadLine());
+
+                List <DiamondMiner> list = new List<DiamondMiner>();
+                
+                for (int i = 0; i < n; i++)
+                {
+                    var data = Console.ReadLine().Split();
+                    DiamondMiner d1 = new DiamondMiner(data[0], data[1], int.Parse(data[2]));
+                    list.Add(d1);
+                }
+                foreach(var d in list)
+                {
+                    d.Izhod();
+                }
+
+                list.OrderByDescending(x => x.TotalNumber).ToList().ForEach(y=>Console.WriteLine($"{y.TotalNumber}"));
+                list.Where(z => z.Name == "A").ToList().ForEach(h => Console.WriteLine($"{h.Name}"));
+                
+            }
+
+
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+           
+        }
+    }
+}
