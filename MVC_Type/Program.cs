67421813495855c288e21514ee26f6ffa3afb12f
VankotU <131190241+VankotU@users.noreply.github.com>
2023-12-12 13:55:47 +0200

    Add files via upload

---
+++ MVC_Type/Program.cs
@@ -0,0 +1,18 @@
+﻿using MVC_Type.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TipController myTip = new TipController();
+            //komentar
+        }
+    }
+}
