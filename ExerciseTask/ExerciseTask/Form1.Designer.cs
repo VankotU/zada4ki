0048caed8b3bd04d7a88ce720eb2eaf7eda3a833
VankotU <131190241+VankotU@users.noreply.github.com>
2024-02-20 09:19:33 +0200

    Add files via upload

---
+++ ExerciseTask/ExerciseTask/Form1.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace ExerciseTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rb_margarita = new System.Windows.Forms.RadioButton();
+            this.rb_kaprichoza = new System.Windows.Forms.RadioButton();
+            this.rb_kalcone = new System.Windows.Forms.RadioButton();
+            this.rb_kutro_stadjone = new System.Windows.Forms.RadioButton();
+            this.rb_pep = new System.Windows.Forms.RadioButton();
+            this.rb_havaiska = new System.Windows.Forms.RadioButton();
+            this.rb_kf = new System.Windows.Forms.RadioButton();
+            this.rb_salsiche = new System.Windows.Forms.RadioButton();
+            this.rb_deep_dish = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rb_malka = new System.Windows.Forms.RadioButton();
+            this.rb_sredna = new System.Windows.Forms.RadioButton();
+            this.rb_golqma = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cb_kechup = new System.Windows.Forms.CheckBox();
+            this.cb_maioneza = new System.Windows.Forms.CheckBox();
+            this.cb_liuto = new System.Windows.Forms.CheckBox();
+            this.cb_parmezan = new System.Windows.Forms.CheckBox();
+            this.cb_chubrika = new System.Windows.Forms.CheckBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btn_poruchka = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rb_margarita
+            // 
+            this.rb_margarita.AutoSize = true;
+            this.rb_margarita.Location = new System.Drawing.Point(6, 28);
+            this.rb_margarita.Name = "rb_margarita";
+            this.rb_margarita.Size = new System.Drawing.Size(69, 17);
+            this.rb_margarita.TabIndex = 0;
+            this.rb_margarita.TabStop = true;
+            this.rb_margarita.Text = "Margarita";
+            this.rb_margarita.UseVisualStyleBackColor = true;
+            // 
+            // rb_kaprichoza
+            // 
+            this.rb_kaprichoza.AutoSize = true;
+            this.rb_kaprichoza.Location = new System.Drawing.Point(6, 52);
+            this.rb_kaprichoza.Name = "rb_kaprichoza";
+            this.rb_kaprichoza.Size = new System.Drawing.Size(78, 17);
+            this.rb_kaprichoza.TabIndex = 1;
+            this.rb_kaprichoza.TabStop = true;
+            this.rb_kaprichoza.Text = "Kparichoza";
+            this.rb_kaprichoza.UseVisualStyleBackColor = true;
+            // 
+            // rb_kalcone
+            // 
+            this.rb_kalcone.AutoSize = true;
+            this.rb_kalcone.Location = new System.Drawing.Point(6, 76);
+            this.rb_kalcone.Name = "rb_kalcone";
+            this.rb_kalcone.Size = new System.Drawing.Size(64, 17);
+            this.rb_kalcone.TabIndex = 2;
+            this.rb_kalcone.TabStop = true;
+            this.rb_kalcone.Text = "Kalcone";
+            this.rb_kalcone.UseVisualStyleBackColor = true;
+            // 
+            // rb_kutro_stadjone
+            // 
+            this.rb_kutro_stadjone.AutoSize = true;
+            this.rb_kutro_stadjone.Location = new System.Drawing.Point(6, 100);
+            this.rb_kutro_stadjone.Name = "rb_kutro_stadjone";
+            this.rb_kutro_stadjone.Size = new System.Drawing.Size(99, 17);
+            this.rb_kutro_stadjone.TabIndex = 3;
+            this.rb_kutro_stadjone.TabStop = true;
+            this.rb_kutro_stadjone.Text = "Kuatro stadjone";
+            this.rb_kutro_stadjone.UseVisualStyleBackColor = true;
+            // 
+            // rb_pep
+            // 
+            this.rb_pep.AutoSize = true;
+            this.rb_pep.Location = new System.Drawing.Point(6, 124);
+            this.rb_pep.Name = "rb_pep";
+            this.rb_pep.Size = new System.Drawing.Size(73, 17);
+            this.rb_pep.TabIndex = 4;
+            this.rb_pep.TabStop = true;
+            this.rb_pep.Text = "Pepperoni";
+            this.rb_pep.UseVisualStyleBackColor = true;
+            // 
+            // rb_havaiska
+            // 
+            this.rb_havaiska.AutoSize = true;
+            this.rb_havaiska.Location = new System.Drawing.Point(6, 148);
+            this.rb_havaiska.Name = "rb_havaiska";
+            this.rb_havaiska.Size = new System.Drawing.Size(70, 17);
+            this.rb_havaiska.TabIndex = 5;
+            this.rb_havaiska.TabStop = true;
+            this.rb_havaiska.Text = "Havaiska";
+            this.rb_havaiska.UseVisualStyleBackColor = true;
+            // 
+            // rb_kf
+            // 
+            this.rb_kf.AutoSize = true;
+            this.rb_kf.Location = new System.Drawing.Point(6, 172);
+            this.rb_kf.Name = "rb_kf";
+            this.rb_kf.Size = new System.Drawing.Size(99, 17);
+            this.rb_kf.TabIndex = 6;
+            this.rb_kf.TabStop = true;
+            this.rb_kf.Text = "Kuatro formadjo";
+            this.rb_kf.UseVisualStyleBackColor = true;
+            // 
+            // rb_salsiche
+            // 
+            this.rb_salsiche.AutoSize = true;
+            this.rb_salsiche.Location = new System.Drawing.Point(6, 196);
+            this.rb_salsiche.Name = "rb_salsiche";
+            this.rb_salsiche.Size = new System.Drawing.Size(65, 17);
+            this.rb_salsiche.TabIndex = 7;
+            this.rb_salsiche.TabStop = true;
+            this.rb_salsiche.Text = "Salsiche";
+            this.rb_salsiche.UseVisualStyleBackColor = true;
+            // 
+            // rb_deep_dish
+            // 
+            this.rb_deep_dish.AutoSize = true;
+            this.rb_deep_dish.Location = new System.Drawing.Point(6, 220);
+            this.rb_deep_dish.Name = "rb_deep_dish";
+            this.rb_deep_dish.Size = new System.Drawing.Size(73, 17);
+            this.rb_deep_dish.TabIndex = 8;
+            this.rb_deep_dish.TabStop = true;
+            this.rb_deep_dish.Text = "Deep dish";
+            this.rb_deep_dish.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rb_deep_dish);
+            this.groupBox1.Controls.Add(this.rb_salsiche);
+            this.groupBox1.Controls.Add(this.rb_kf);
+            this.groupBox1.Controls.Add(this.rb_havaiska);
+            this.groupBox1.Controls.Add(this.rb_pep);
+            this.groupBox1.Controls.Add(this.rb_kutro_stadjone);
+            this.groupBox1.Controls.Add(this.rb_kalcone);
+            this.groupBox1.Controls.Add(this.rb_kaprichoza);
+            this.groupBox1.Controls.Add(this.rb_margarita);
+            this.groupBox1.Location = new System.Drawing.Point(7, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(114, 248);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Izbor na pizza";
+            // 
+            // rb_malka
+            // 
+            this.rb_malka.AutoSize = true;
+            this.rb_malka.Location = new System.Drawing.Point(13, 30);
+            this.rb_malka.Name = "rb_malka";
+            this.rb_malka.Size = new System.Drawing.Size(53, 17);
+            this.rb_malka.TabIndex = 10;
+            this.rb_malka.TabStop = true;
+            this.rb_malka.Text = "malka";
+            this.rb_malka.UseVisualStyleBackColor = true;
+            // 
+            // rb_sredna
+            // 
+            this.rb_sredna.AutoSize = true;
+            this.rb_sredna.Location = new System.Drawing.Point(13, 53);
+            this.rb_sredna.Name = "rb_sredna";
+            this.rb_sredna.Size = new System.Drawing.Size(57, 17);
+            this.rb_sredna.TabIndex = 11;
+            this.rb_sredna.TabStop = true;
+            this.rb_sredna.Text = "sredna";
+            this.rb_sredna.UseVisualStyleBackColor = true;
+            // 
+            // rb_golqma
+            // 
+            this.rb_golqma.AutoSize = true;
+            this.rb_golqma.Location = new System.Drawing.Point(13, 76);
+            this.rb_golqma.Name = "rb_golqma";
+            this.rb_golqma.Size = new System.Drawing.Size(59, 17);
+            this.rb_golqma.TabIndex = 12;
+            this.rb_golqma.TabStop = true;
+            this.rb_golqma.Text = "golqma";
+            this.rb_golqma.UseVisualStyleBackColor = true;
+            this.rb_golqma.CheckedChanged += new System.EventHandler(this.radioButton12_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rb_golqma);
+            this.groupBox2.Controls.Add(this.rb_sredna);
+            this.groupBox2.Controls.Add(this.rb_malka);
+            this.groupBox2.Location = new System.Drawing.Point(144, 23);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(82, 100);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Razmer";
+            // 
+            // cb_kechup
+            // 
+            this.cb_kechup.AutoSize = true;
+            this.cb_kechup.Location = new System.Drawing.Point(22, 22);
+            this.cb_kechup.Name = "cb_kechup";
+            this.cb_kechup.Size = new System.Drawing.Size(63, 17);
+            this.cb_kechup.TabIndex = 14;
+            this.cb_kechup.Text = "Kechup";
+            this.cb_kechup.UseVisualStyleBackColor = true;
+            // 
+            // cb_maioneza
+            // 
+            this.cb_maioneza.AutoSize = true;
+            this.cb_maioneza.Location = new System.Drawing.Point(22, 44);
+            this.cb_maioneza.Name = "cb_maioneza";
+            this.cb_maioneza.Size = new System.Drawing.Size(72, 17);
+            this.cb_maioneza.TabIndex = 15;
+            this.cb_maioneza.Text = "Maioneza";
+            this.cb_maioneza.UseVisualStyleBackColor = true;
+            // 
+            // cb_liuto
+            // 
+            this.cb_liuto.AutoSize = true;
+            this.cb_liuto.Location = new System.Drawing.Point(22, 68);
+            this.cb_liuto.Name = "cb_liuto";
+            this.cb_liuto.Size = new System.Drawing.Size(49, 17);
+            this.cb_liuto.TabIndex = 16;
+            this.cb_liuto.Text = "Liuto";
+            this.cb_liuto.UseVisualStyleBackColor = true;
+            // 
+            // cb_parmezan
+            // 
+            this.cb_parmezan.AutoSize = true;
+            this.cb_parmezan.Location = new System.Drawing.Point(22, 92);
+            this.cb_parmezan.Name = "cb_parmezan";
+            this.cb_parmezan.Size = new System.Drawing.Size(76, 17);
+            this.cb_parmezan.TabIndex = 17;
+            this.cb_parmezan.Text = "Parmezan ";
+            this.cb_parmezan.UseVisualStyleBackColor = true;
+            // 
+            // cb_chubrika
+            // 
+            this.cb_chubrika.AutoSize = true;
+            this.cb_chubrika.Location = new System.Drawing.Point(22, 116);
+            this.cb_chubrika.Name = "cb_chubrika";
+            this.cb_chubrika.Size = new System.Drawing.Size(68, 17);
+            this.cb_chubrika.TabIndex = 18;
+            this.cb_chubrika.Text = "Chubrika";
+            this.cb_chubrika.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.cb_chubrika);
+            this.groupBox3.Controls.Add(this.cb_parmezan);
+            this.groupBox3.Controls.Add(this.cb_liuto);
+            this.groupBox3.Controls.Add(this.cb_maioneza);
+            this.groupBox3.Controls.Add(this.cb_kechup);
+            this.groupBox3.Location = new System.Drawing.Point(248, 24);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(107, 140);
+            this.groupBox3.TabIndex = 19;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Podpravki";
+            // 
+            // btn_poruchka
+            // 
+            this.btn_poruchka.Location = new System.Drawing.Point(7, 314);
+            this.btn_poruchka.Name = "btn_poruchka";
+            this.btn_poruchka.Size = new System.Drawing.Size(114, 41);
+            this.btn_poruchka.TabIndex = 20;
+            this.btn_poruchka.Text = "Poruchka";
+            this.btn_poruchka.UseVisualStyleBackColor = true;
+            this.btn_poruchka.Click += new System.EventHandler(this.btn_poruchka_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Margarita",
+            "Kaprichoza",
+            "Klacone",
+            "Kuatro stadjone",
+            "Peperoni",
+            "Havaiska",
+            "Kuatro formadjo",
+            "Salsiche",
+            "Deep dish"});
+            this.comboBox1.Location = new System.Drawing.Point(7, 287);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(159, 21);
+            this.comboBox1.TabIndex = 21;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(173, 314);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(106, 40);
+            this.btn_clear.TabIndex = 22;
+            this.btn_clear.Text = "Izchisti";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 376);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btn_poruchka);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Pizzeria Mania";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rb_margarita;
+        private System.Windows.Forms.RadioButton rb_kaprichoza;
+        private System.Windows.Forms.RadioButton rb_kalcone;
+        private System.Windows.Forms.RadioButton rb_kutro_stadjone;
+        private System.Windows.Forms.RadioButton rb_pep;
+        private System.Windows.Forms.RadioButton rb_havaiska;
+        private System.Windows.Forms.RadioButton rb_kf;
+        private System.Windows.Forms.RadioButton rb_salsiche;
+        private System.Windows.Forms.RadioButton rb_deep_dish;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rb_malka;
+        private System.Windows.Forms.RadioButton rb_sredna;
+        private System.Windows.Forms.RadioButton rb_golqma;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox cb_kechup;
+        private System.Windows.Forms.CheckBox cb_maioneza;
+        private System.Windows.Forms.CheckBox cb_liuto;
+        private System.Windows.Forms.CheckBox cb_parmezan;
+        private System.Windows.Forms.CheckBox cb_chubrika;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btn_poruchka;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btn_clear;
+    }
+}
+
---
+++ ExerciseTask/ExerciseTask/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExerciseTask
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton12_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_poruchka_Click(object sender, EventArgs e)
+        {
+            string typePizza=comboBox1.Text;
+            string sizePizza="";
+            string extras="";
+
+            /*if (rb_margarita.Checked)
+            {
+                typePizza = "Margarita";
+            }
+            if (rb_kaprichoza.Checked)
+            {
+                typePizza = "Kaprichoza";
+            }
+            if (rb_kalcone.Checked)
+            {
+                typePizza = "Kalcone";
+            }
+            if (rb_kutro_stadjone.Checked)
+            {
+                typePizza = "Kuatro stadhone";
+            }
+            if (rb_pep.Checked)
+            {
+                typePizza = "Pepperoni";
+            }
+            if (rb_havaiska.Checked)
+            {
+                typePizza = "Havaiska";
+            }
+            if (rb_kf.Checked)
+            {
+                typePizza = "Kuatro formadjo";
+            }
+            if (rb_salsiche.Checked)
+            {
+                typePizza = "Salsiche";
+            }
+            if (rb_deep_dish.Checked)
+            {
+                typePizza = "Deep dish";
+            }*/
+
+            if (rb_malka.Checked)
+            {
+                sizePizza="malka";
+            }
+            if (rb_sredna.Checked)
+            {
+                sizePizza="sredna";
+            }
+            if (rb_golqma.Checked)
+            {
+                sizePizza="golqma";
+            }
+
+            if (cb_chubrika.Checked)
+            {
+                extras = cb_chubrika.Text;
+            }
+           /* else
+            {
+                extras ="";
+            }*/
+
+            if (cb_kechup.Checked)
+            {
+                extras = cb_kechup.Text;
+            }
+           /* else
+            {
+                extras ="";
+            }*/
+
+            if (cb_liuto.Checked)
+            {
+                extras = cb_liuto.Text;
+            }
+           /* else
+            {
+                extras ="";
+            }*/
+
+            if (cb_maioneza.Checked)
+            {
+                extras = cb_maioneza.Text;
+            }
+            /*else
+            {
+                extras ="";
+            }*/
+
+            if (cb_parmezan.Checked)
+            {
+                extras = cb_parmezan.Text;
+            }
+            /*else 
+            {
+                extras ="";
+            }*/
+
+            if (sizePizza=="")
+            {
+                MessageBox.Show("Ne si vuvel razmer na picata");
+                rb_malka.Checked = true;
+                return;
+            }
+            if (typePizza=="")
+            {
+                MessageBox.Show("Ne si vuvel pizza");
+                rb_margarita.Checked=true;
+                return;
+            }
+            if (string.IsNullOrEmpty(extras))
+            {
+                extras="nqma podpravki";
+            }
+
+            DialogResult result = MessageBox.Show($"Vie izbrahte {sizePizza} pica {typePizza}, Potvurjdavate li?","Potvurjdavane",MessageBoxButtons.YesNoCancel);
+
+            if (result==DialogResult.Yes)
+            {
+                MessageBox.Show("Izprateno");
+                Application.Restart();
+            }
+            if (result==DialogResult.No)
+            {
+                Application.Restart();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
