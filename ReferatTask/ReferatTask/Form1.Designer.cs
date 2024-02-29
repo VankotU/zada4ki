98454d66348e1c27ea04234ed1f5a550383e8f95
VankotU <131190241+VankotU@users.noreply.github.com>
2024-02-29 18:42:35 +0200

    Add files via upload

---
+++ ReferatTask/ReferatTask/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace ReferatTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.txtBox_Number = new System.Windows.Forms.TextBox();
+            this.lbl_Number = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 151);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(200, 27);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 0;
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(12, 65);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(200, 56);
+            this.btn_Start.TabIndex = 1;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // txtBox_Number
+            // 
+            this.txtBox_Number.Location = new System.Drawing.Point(12, 39);
+            this.txtBox_Number.Name = "txtBox_Number";
+            this.txtBox_Number.Size = new System.Drawing.Size(100, 20);
+            this.txtBox_Number.TabIndex = 2;
+            // 
+            // lbl_Number
+            // 
+            this.lbl_Number.AutoSize = true;
+            this.lbl_Number.Location = new System.Drawing.Point(12, 23);
+            this.lbl_Number.Name = "lbl_Number";
+            this.lbl_Number.Size = new System.Drawing.Size(44, 13);
+            this.lbl_Number.TabIndex = 3;
+            this.lbl_Number.Text = "Number";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(237, 222);
+            this.Controls.Add(this.lbl_Number);
+            this.Controls.Add(this.txtBox_Number);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.TextBox txtBox_Number;
+        private System.Windows.Forms.Label lbl_Number;
+    }
+}
+
---
+++ ReferatTask/ReferatTask/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReferatTask
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse(txtBox_Number.Text);
+            if (number >= 0 && number <= 100 && number % 10 == 0)
+            {
+                progressBar1.Value = number;
+            }
+            else
+            {
+                MessageBox.Show("Невалидно число. Моля, въведете число между 0 и 100, което се дели на 10 без остатък.");
+                txtBox_Number.Focus();
+                return;
+            }
+        }
+    }
+}
