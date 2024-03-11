1698c3e1c87ee8b7fa1cfe5a37b05dbba919ea21
VankotU <131190241+VankotU@users.noreply.github.com>
2024-03-11 09:54:58 +0200

    Add files via upload

---
+++ BookStore/BookStore/Book.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore
+{
+    public class Book:IComparable<Book>
+    {
+        private string title;
+        private string author;
+        private int year;
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+        public Book(string author, string title, int year) 
+        {
+            this.Title = title;
+            this.Author = author;
+            this.Year = year;
+        }
+        public int CompareTo(Book other) 
+        {
+            int result = this.Year.CompareTo(other.Year);
+            if (result == 0) 
+            {
+                result = this.Title.CompareTo(other.Title);
+            }
+            return result;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Title - {title}, Author - {author}, Year - {Year}");
+        }
+    }
+}
---
+++ BookStore/BookStore/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kolko knigi?");
+            int count = int.Parse(Console.ReadLine());
+            Console.WriteLine("Danni za knigite?");
+            List<Book> list = new List<Book>();
+            for (int i = 0; i < count; i++) 
+            {
+                var data = Console.ReadLine().Split();
+                Book b = new Book(data[0], data[1], int.Parse(data[2]));
+                list.Add(b);
+            }
+            foreach (var book in list)
+            {
+                book.Print();
+            }
+            BookComparator comparator = new BookComparator();
+            list.Sort(comparator);
+            Console.WriteLine(string.Join(Environment.NewLine, list.Select(x=>x.Year)));
+        }
+    }
+}
