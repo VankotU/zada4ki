13d669836dd7a0cf0e14d48cff244757f3a73367
VankotU <131190241+VankotU@users.noreply.github.com>
2024-06-04 22:30:09 +0300

    Add files via upload

--- ProjectVeganStore/ProjectVeganStore/Form1.Designer.cs
+++ ProjectVeganStore/ProjectVeganStore/Form1.Designer.cs
@@ -28,13 +28,210 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txt_Id = new System.Windows.Forms.TextBox();
+            this.txt_Name = new System.Windows.Forms.TextBox();
+            this.txt_Description = new System.Windows.Forms.TextBox();
+            this.cmb_Type = new System.Windows.Forms.ComboBox();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_Update = new System.Windows.Forms.Button();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.btn_Find = new System.Windows.Forms.Button();
+            this.btn_SelectAll = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_Price = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_Id
+            // 
+            this.txt_Id.Location = new System.Drawing.Point(81, 43);
+            this.txt_Id.Name = "txt_Id";
+            this.txt_Id.Size = new System.Drawing.Size(100, 20);
+            this.txt_Id.TabIndex = 0;
+            // 
+            // txt_Name
+            // 
+            this.txt_Name.Location = new System.Drawing.Point(81, 69);
+            this.txt_Name.Name = "txt_Name";
+            this.txt_Name.Size = new System.Drawing.Size(100, 20);
+            this.txt_Name.TabIndex = 1;
+            // 
+            // txt_Description
+            // 
+            this.txt_Description.Location = new System.Drawing.Point(81, 95);
+            this.txt_Description.Name = "txt_Description";
+            this.txt_Description.Size = new System.Drawing.Size(100, 20);
+            this.txt_Description.TabIndex = 2;
+            // 
+            // cmb_Type
+            // 
+            this.cmb_Type.FormattingEnabled = true;
+            this.cmb_Type.Location = new System.Drawing.Point(81, 153);
+            this.cmb_Type.Name = "cmb_Type";
+            this.cmb_Type.Size = new System.Drawing.Size(121, 21);
+            this.cmb_Type.TabIndex = 3;
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Location = new System.Drawing.Point(45, 180);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(75, 23);
+            this.btn_Add.TabIndex = 4;
+            this.btn_Add.Text = "Add";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // btn_Update
+            // 
+            this.btn_Update.Location = new System.Drawing.Point(126, 180);
+            this.btn_Update.Name = "btn_Update";
+            this.btn_Update.Size = new System.Drawing.Size(75, 23);
+            this.btn_Update.TabIndex = 5;
+            this.btn_Update.Text = "Update";
+            this.btn_Update.UseVisualStyleBackColor = true;
+            this.btn_Update.Click += new System.EventHandler(this.btn_Update_Click);
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Location = new System.Drawing.Point(207, 180);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(75, 23);
+            this.btn_Delete.TabIndex = 6;
+            this.btn_Delete.Text = "Delete";
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
+            // 
+            // btn_Find
+            // 
+            this.btn_Find.Location = new System.Drawing.Point(91, 209);
+            this.btn_Find.Name = "btn_Find";
+            this.btn_Find.Size = new System.Drawing.Size(75, 23);
+            this.btn_Find.TabIndex = 7;
+            this.btn_Find.Text = "Find";
+            this.btn_Find.UseVisualStyleBackColor = true;
+            this.btn_Find.Click += new System.EventHandler(this.btn_Find_Click);
+            // 
+            // btn_SelectAll
+            // 
+            this.btn_SelectAll.Location = new System.Drawing.Point(172, 209);
+            this.btn_SelectAll.Name = "btn_SelectAll";
+            this.btn_SelectAll.Size = new System.Drawing.Size(75, 23);
+            this.btn_SelectAll.TabIndex = 8;
+            this.btn_SelectAll.Text = "Select All";
+            this.btn_SelectAll.UseVisualStyleBackColor = true;
+            this.btn_SelectAll.Click += new System.EventHandler(this.btn_SelectAll_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(365, 43);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(276, 186);
+            this.listBox1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Description";
+            // 
+            // txt_Price
+            // 
+            this.txt_Price.Location = new System.Drawing.Point(81, 121);
+            this.txt_Price.Name = "txt_Price";
+            this.txt_Price.Size = new System.Drawing.Size(100, 20);
+            this.txt_Price.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Price";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(4, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Type";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(653, 325);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_Price);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btn_SelectAll);
+            this.Controls.Add(this.btn_Find);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.btn_Update);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.cmb_Type);
+            this.Controls.Add(this.txt_Description);
+            this.Controls.Add(this.txt_Name);
+            this.Controls.Add(this.txt_Id);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txt_Id;
+        private System.Windows.Forms.TextBox txt_Name;
+        private System.Windows.Forms.TextBox txt_Description;
+        private System.Windows.Forms.ComboBox cmb_Type;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Button btn_Update;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.Button btn_Find;
+        private System.Windows.Forms.Button btn_SelectAll;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_Price;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- ProjectVeganStore/ProjectVeganStore/Form1.cs
+++ ProjectVeganStore/ProjectVeganStore/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ProjectVeganStore.Controller;
+using ProjectVeganStore.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,164 @@ namespace ProjectVeganStore
 {
     public partial class Form1 : Form
     {
+        VeganController veganController = new VeganController();
+        VeganTypeController veganTypeController = new VeganTypeController();
         public Form1()
         {
             InitializeComponent();
+        }
+        private void LoadRecord(Vegans vegan)
+        {
+            txt_Id.BackColor = Color.White;
+            txt_Id.Text = vegan.Id.ToString();
+            txt_Name.Text = vegan.Name;
+            txt_Description.Text = vegan.Description;
+            txt_Price.Text = vegan.Price.ToString();
+            cmb_Type.Text = vegan.Types.Name;
+        }
+        private void ClearScreen()
+        {
+            txt_Id.BackColor = Color.White;
+            txt_Id.Clear();
+            txt_Name.Clear();
+            txt_Description.Clear();
+            txt_Price.Clear();
+            cmb_Type.Text = "";
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            List<VeganType> allVeganTypes = veganTypeController.GetAllTypes();
+            cmb_Type.DataSource = allVeganTypes;
+            cmb_Type.DisplayMember = "Name";
+            cmb_Type.ValueMember = "Id";
+            btn_SelectAll_Click(sender, e);
+        }
+
+        private void btn_SelectAll_Click(object sender, EventArgs e)
+        {
+            List<Vegans> allVegans = veganController.GetAll();
+            listBox1.Items.Clear();
+            foreach (var item in allVegans)
+            {
+                listBox1.Items.Add($"{item.Id}.Name - {item.Name} - Price: {item.Price}, Description - {item.Description}, Type - {item.Types.Name}");
+            }
+        }
+
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txt_Name.Text) || txt_Name.Text == "")
+            {
+                MessageBox.Show("Insert data!");
+                txt_Name.Focus();
+                return;
+            }
+            Vegans newVegans = new Vegans();
+            newVegans.Price = double.Parse(txt_Price.Text);
+            newVegans.Name = txt_Name.Text;
+            newVegans.Description = txt_Description.Text;
+            newVegans.Vegan_type_Id = (int)cmb_Type.SelectedValue;
+
+            veganController.Create(newVegans);
+            MessageBox.Show("Item successfully added!");
+            ClearScreen();
+            btn_SelectAll_Click(sender, e);
+        }
+
+        private void btn_Find_Click(object sender, EventArgs e)
+        {
+            int findId = 0;
+            if (string.IsNullOrEmpty(txt_Id.Text) || txt_Id.Text.All(char.IsDigit))
+            {
+                MessageBox.Show("You must insert ID for searching!");
+                txt_Id.BackColor = Color.Red;
+                txt_Id.Focus();
+                return;
+            }
+            else
+            {
+                findId = int.Parse(txt_Id.Text);
+            }
+            Vegans findedVegan = veganController.Get(findId);
+            if (findedVegan == null)
+            {
+                MessageBox.Show("There isn't such item in Database \n Insert ID for search!");
+                txt_Id.BackColor = Color.Red;
+                txt_Id.Focus();
+                return;
+            }
+            LoadRecord(findedVegan);
+        }
+
+        private void btn_Update_Click(object sender, EventArgs e)
+        {
+            int findId = 0;
+            if (string.IsNullOrEmpty(txt_Id.Text) || txt_Id.Text.All(char.IsDigit))
+            {
+                MessageBox.Show("You must insert ID for searching!");
+                txt_Id.BackColor = Color.Red;
+                txt_Id.Focus();
+                return;
+            }
+            else
+            {
+                findId = int.Parse(txt_Id.Text);
+            }
+            if (string.IsNullOrEmpty(txt_Name.Text))
+            {
+                Vegans findedVegan = veganController.Get(findId);
+                if (findedVegan == null)
+                {
+                    MessageBox.Show("There isn't such item in Database \n Insert ID for search!");
+                    txt_Id.BackColor = Color.Red;
+                    txt_Id.Focus();
+                    return;
+                }
+                LoadRecord(findedVegan);
+            }
+            else
+            {
+                Vegans updatedVegan = new Vegans();
+                updatedVegan.Name = txt_Name.Text;
+                updatedVegan.Description = txt_Description.Text;
+                updatedVegan.Price = double.Parse(txt_Price.Text);
+                updatedVegan.Vegan_type_Id = (int)cmb_Type.SelectedValue;
+
+                veganController.Update(findId, updatedVegan);
+            }
+            btn_SelectAll_Click(sender, e);
+        }
+
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+            int findId = 0;
+            if (string.IsNullOrEmpty(txt_Id.Text) || txt_Id.Text.All(char.IsDigit))
+            {
+                MessageBox.Show("You must insert ID for searching!");
+                txt_Id.BackColor = Color.Red;
+                txt_Id.Focus();
+                return;
+            }
+            else
+            {
+                findId = int.Parse(txt_Id.Text);
+            }
+            Vegans findedVegan = veganController.Get(findId);
+            if (findedVegan == null)
+            {
+                MessageBox.Show("There isn't such item in Database \n Insert ID for search!");
+                txt_Id.BackColor = Color.Red;
+                txt_Id.Focus();
+                return;
+            }
+            LoadRecord(findedVegan);
+
+            DialogResult answer = MessageBox.Show("Do you really want to delete item number "+findId+" ?", "PROMPT", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (answer == DialogResult.Yes)
+            {
+                veganController.Delete(findId);
+            }
+            btn_SelectAll_Click(sender, e);
         }
     }
 }
